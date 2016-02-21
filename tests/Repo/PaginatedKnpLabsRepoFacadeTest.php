24bffa066b677998a592591617ffc24dbe92c7da
Miro Svrtan <miro@mirosvrtan.me>
2016-02-21 16:19:13 +0100

    Adding methods to fetch repo details, branches, tags, pull requests, commits & commit statuses

--- tests/Repo/PaginatedKnpLabsRepoFacadeTest.php
+++ tests/Repo/PaginatedKnpLabsRepoFacadeTest.php
@@ -10,48 +10,137 @@ use Mockery as m;
  */
 class PaginatedKnpLabsRepoFacadeTest extends \PHPUnit_Framework_TestCase
 {
+    private $facade;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->facade = new PaginatedKnpLabsRepoFacade(
+            $this->getTokenAuthenticatedApiClient(),
+            $this->provideTestRepo()
+        );
+    }
+
+    /**
+     * @group GithubIntegration
+     * @group Live
+     */
+    public function testFetchRepoDetails()
+    {
+        $result = $this->facade->fetchDetails();
+
+        self::assertSame($this->provideTestRepo()->getFullName(), $result['full_name']);
+    }
+
+    /**
+     * @group GithubIntegration
+     * @group Live
+     */
+    public function testFetchBranch()
+    {
+        $result = $this->facade->fetchBranch('master');
+
+        self::assertSame('master', $result['name']);
+    }
+
+    /**
+     * @group GithubIntegration
+     * @group Live
+     */
+    public function testFetchAllBranches()
+    {
+        self::assertCount(7, $this->facade->fetchAllBranches());
+    }
+
+    /**
+     * @group GithubIntegration
+     * @group Live
+     */
+    public function testFetchAllBranchNames()
+    {
+        self::assertCount(7, $this->facade->fetchAllBranchNames());
+    }
+
+    /**
+     * @group GithubIntegration
+     * @group Live
+     */
+    public function testFetchAllTagNames()
+    {
+        self::assertCount(1, $this->facade->fetchAllTagNames());
+    }
+
+    /**
+     * @group GithubIntegration
+     * @group Live
+     */
+    public function testFetchCommit()
+    {
+        $result = $this->facade->fetchCommit('db911bd3a3dd8bb2ad9eccbcb0a396595a51491d');
+
+        self::assertSame('db911bd3a3dd8bb2ad9eccbcb0a396595a51491d', $result['sha']);
+    }
+
+    /**
+     * @group GithubIntegration
+     * @group Live
+     * @group wip
+     */
+    public function testFetchCommitStatuses()
+    {
+        self::assertCount(27, $this->facade->fetchCommitStatuses('db911bd3a3dd8bb2ad9eccbcb0a396595a51491d'));
+    }
+
+    /**
+     * @group GithubIntegration
+     * @group Live
+     * @group wip
+     */
+    public function testFetchCommitStatus()
+    {
+        $result = $this->facade->fetchCommitStatus('db911bd3a3dd8bb2ad9eccbcb0a396595a51491d');
+        self::assertSame(
+            'https://api.github.com/repos/devboard/test-hitman/commits/db911bd3a3dd8bb2ad9eccbcb0a396595a51491d',
+            $result['commit_url']
+        );
+    }
+
+    /**
+     * @group GithubIntegration
+     * @group Live
+     * @group wip
+     */
+    public function testFetchAllPullRequests()
+    {
+        self::assertCount(2, $this->facade->fetchAllPullRequests());
+    }
+
     /**
      * @group GithubIntegration
      * @group Live
      */
     public function testFetchAllMilestones()
     {
-        $target = new PaginatedKnpLabsRepoFacade(
-            $this->getTokenAuthenticatedApiClient(),
-            $this->provideTestRepo()
-        );
-
-        self::assertCount(4, $target->fetchAllMilestones());
+        self::assertCount(4, $this->facade->fetchAllMilestones());
     }
 
     /**
      * @group GithubIntegration
      * @group Live
-     * @group wip
      */
     public function testFetchAllIssues()
     {
-        $target = new PaginatedKnpLabsRepoFacade(
-            $this->getTokenAuthenticatedApiClient(),
-            $this->provideTestRepo()
-        );
-
-        self::assertCount(10, $target->fetchAllIssues());
+        self::assertCount(10, $this->facade->fetchAllIssues());
     }
 
     /**
      * @group GithubIntegration
      * @group Live
-     * @group wip
      */
     public function testFetchAllIssuesAndPullRequests()
     {
-        $target = new PaginatedKnpLabsRepoFacade(
-            $this->getTokenAuthenticatedApiClient(),
-            $this->provideTestRepo()
-        );
-
-        self::assertCount(12, $target->fetchAllIssuesAndPullRequests());
+        self::assertCount(12, $this->facade->fetchAllIssuesAndPullRequests());
     }
 
     /**
@@ -72,6 +161,7 @@ class PaginatedKnpLabsRepoFacadeTest extends \PHPUnit_Framework_TestCase
         $githubRepo = m::mock('DevBoardLib\GithubCore\Repo\GithubRepo');
         $githubRepo->shouldReceive('getOwner')->andReturn('devboard');
         $githubRepo->shouldReceive('getName')->andReturn('test-hitman');
+        $githubRepo->shouldReceive('getFullName')->andReturn('devboard/test-hitman');
 
         return $githubRepo;
     }
