29cf6bcdae2d085463c191fb6dd9c4b6b8762c86
Miro Svrtan <miro@mirosvrtan.me>
2016-02-21 21:35:49 +0100

    WIP

--- src/Repo/RepoFacade.php
+++ src/Repo/RepoFacade.php
@@ -6,6 +6,24 @@ namespace DevBoardLib\GithubApiFacade\Repo;
  */
 interface RepoFacade
 {
+    public function fetchDetails();
+
+    public function fetchBranch($branchName);
+
+    public function fetchAllBranches();
+
+    public function fetchAllBranchNames();
+
+    public function fetchAllTagNames();
+
+    public function fetchCommit($commitSha);
+
+    public function fetchCommitStatus($commitSha);
+
+    public function fetchCommitStatuses($commitSha);
+
+    public function fetchAllPullRequests();
+
     public function fetchAllMilestones();
 
     public function fetchAllIssues();
