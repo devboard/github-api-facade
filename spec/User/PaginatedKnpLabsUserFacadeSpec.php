35190c695974f57b9306cfe33a79291b205ba76b
Miro Svrtan <miro@mirosvrtan.me>
2016-05-27 11:18:31 +0200

    Defining strict_types on all PHP files

--- spec/User/PaginatedKnpLabsUserFacadeSpec.php
+++ spec/User/PaginatedKnpLabsUserFacadeSpec.php
@@ -1,5 +1,6 @@
 <?php
 
+declare (strict_types = 1);
 namespace spec\DevBoardLib\GithubApiFacade\User;
 
 use Github\Client;
--- tests/Repo/PaginatedKnpLabsRepoFacadeTest.php
+++ tests/Repo/PaginatedKnpLabsRepoFacadeTest.php
@@ -1,5 +1,6 @@
 <?php
 
+declare (strict_types = 1);
 namespace tests\DevBoardLib\GithubApiFacade\Repo;
 
 use DevBoardLib\GithubApiFacade\Client\KnpLabsClientFactory;
